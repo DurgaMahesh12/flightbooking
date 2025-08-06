5291f2e3053b0db468b6c2d73eb3cb50b445ecf1
HP <durgamaheshvanum44@gmail.com>
2025-08-06 13:58:18 +0530

    new changes

---
+++ src/test/java/com/testcases/FlightBookingTests2.java
@@ -0,0 +1,40 @@
+package com.testcases;
+
+import com.pageobjects.Homepage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FlightBookingTests2 extends com.base.baseclass {
+
+	public WebDriver driver;
+	public WebDriverWait wait;
+
+	@BeforeMethod
+	public void Navigatetowebsite() {
+		driver = InitializeBrowser("chrome");
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
+
+	@Test
+	public void Searchflights() throws InterruptedException {
+		Homepage homepage = new Homepage(driver);
+		Thread.sleep(2000);
+		homepage.closepopup();
+		wait.until(ExpectedConditions.visibilityOf(homepage.getSourceCityElement()));
+		homepage.entersourcecity("Pune");
+
+	}
+
+	@AfterMethod
+	public void close() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
